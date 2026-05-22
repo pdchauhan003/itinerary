30833c5ae471803119bebdb8e8da3cb86f12934e
pdchauhan003 <pdchauhan003@gmail.com>
2026-05-22 12:04:59 +0530

    add getfiles controller

--- backend/src/controllers/upload.controller.js
+++ backend/src/controllers/upload.controller.js
@@ -1,49 +1,4 @@
-// import { cloudinary } from "../utils/cloudinary.js";
-
-// export const uploadSingleImage=async(req,res)=>{
-//     try{
-//         if(!req.file){
-//             return res.status(400).json({success:false,message:'file not found'});
-//         }
-//         const result=await cloudinary.uploader.upload(
-//             req.file.path,
-//             {
-//                 folder: 'single_uploads'
-//             }
-//         );
-//         res.status(200).json({success: true,message: 'Image uploaded successfully',imageUrl: result.secure_url});
-
-//     }
-//     catch(error){
-//         console.log('error in upoad ingle image in cloudinary');
-//         res.status(500).json({success: false,message: 'Server Error'});
-//     }
-// }
-
-// export const uploadMultipleImages=async(req,res)=>{
-//     try{
-//         if(!req.files || req.files.length==0){
-//             return res.status(400).json({success: false,message: 'Images not found'});
-//         }
-//         const imageUrls = [];
-//         for (const file of req.files) {
-//             const result = await cloudinary.uploader.upload(
-//                 file.path,
-//                 {
-//                     folder: 'multiple_uploads'
-//                 }
-//             );
-//             imageUrls.push(result.secure_url);
-//         }
-//         res.status(200).json({success: true,message: 'Images uploaded successfully',images: imageUrls});
-//     }
-//     catch(error){
-//         console.log('error in upoad ingle image in cloudinary');
-//         res.status(500).json({success: false,message: 'Server Error'});
-//     }
-// }
-
-
+import { Upload } from "../models/uploads.js";
 import { uploadMultipleFilesService } from "../services/upload.service.js";
 
 export const uploadDocument=async(req,res)=>{
@@ -60,5 +15,17 @@ export const uploadDocument=async(req,res)=>{
     catch(error){
         console.log(error);
         return res.status(500).json({success: false,message: 'Server Error'});
+    }
+}
+
+export const getUserUploadedFiles=async(req,res)=>{
+    try{
+        const userId=req.user._id;
+        const uploads=await Upload.find({user:userId}).sort({createdAt:-1});
+        return res.status(200).json({success:true,message:'file finds success'})
+    }
+    catch(error){
+        console.log('error top get uploaded file by user')
+        return res.status(500),json({success:false,message:'error top get uploaded file by user'})
     }
 }
--- backend/src/routes/upload.route.js
+++ backend/src/routes/upload.route.js
@@ -1,18 +1,14 @@
 import express from 'express';
 // import { uploadSingleImage,uploadMultipleImages } from "../controllers/upload.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { uploadDocument } from '../controllers/upload.controller.js';
+import { uploadDocument,getUserUploadedFiles } from '../controllers/upload.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
+
 
 const uploadRouter=express.Router();
 
-// uploadRouter.post('/single',upload.single('image'),uploadSingleImage);
-// uploadRouter.post('/multiple',upload.array('images', 5),uploadMultipleImages);
-
 uploadRouter.post('/',protect,upload.single('file'),uploadDocument);
-
-
-// Multiple files upload
-uploadRouter.post('/multiple',protect,upload.array('files', 5),uploadDocument);
+uploadRouter.post('/multiple',protect,upload.array('files', 5),uploadDocument);  // Multiple files upload
+uploadRouter.get('/',protect,getUserUploadedFiles)
 
 export {uploadRouter};
