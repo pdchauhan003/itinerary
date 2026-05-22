8c2de40eb5f651f63973f0c66f8c92bf5ffb06a7
pdchauhan003 <pdchauhan003@gmail.com>
2026-05-22 15:11:41 +0530

    update itinerary model

--- backend/src/models/itinerary.js
+++ backend/src/models/itinerary.js
@@ -1,66 +1,18 @@
-import mg, { model } from 'mongoose';
-import bcrypt from 'bcrypt';
+import mg from "mongoose";
 
-const itinerarySchema=new mg.Schema({
-    user:{type:mg.Schema.Types.ObjectId,ref:'User',required:true},
-    title:{type:String},
-    uploadedFiles:[{
-        fileName:String,
-        fileUrl:String,
-        fileType:{type:String,enum:['pdf','image']},
-        uploadedAt:{type:String,default:Date.now}
-    }],
-    tripInfo:{
-        from: String,
-        to: String,
-        startDate: Date,
-        endDate: Date,
-        travelers: {
-        type: Number,
-        default: 1,
-        },
-    },
-
-    extractedData: {
-        rawText: String,
-        structuredData: {
-        type: Object,
-        default: {},
-        },
-    },
-
-    aiGeneratedItinerary: {
-        type: Object,
-        default: null,
-    },
-
-    status: {
-      type: String,
-      enum: ["uploaded", "processing", "completed", "failed"],
-      default: "uploaded",
-    },
-
-    shareId: {
-      type: String,
-      unique: true,
-      sparse: true,
-    },
-
-    isShared: {
-      type: Boolean,
-      default: false,
-    },
-
-    metadata: {
-      aiModel: String, // "gemini-1.5", "gpt-4o"
-      processingTime: Number, // in ms
-    },
-},
+const itinerarySchema = new mg.Schema(
+  {
+    user: {type: mg.Schema.Types.ObjectId,ref: "User",required: true,},
+    title: {type: String,required: true,trim: true,},
+    itineraryData: {type: String, required: true,},
+    pdfUrl: String,
+    uploads: [{type: mg.Schema.Types.ObjectId,ref: "Upload",},],
+  },
   {
     timestamps: true,
   }
 );
 
-const Itinerary=mg.model('Itinerary',itinerarySchema);
+const Itinerary = mg.model("Itinerary", itinerarySchema);
 
-export {Itinerary};
+export { Itinerary };
